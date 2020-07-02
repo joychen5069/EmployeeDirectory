0350950adc58dfeb23ea9677a3570906b88a25ae
Joy Chen <joychen5069@gmail.com>
2020-07-02 14:01:32 -0400

    first name in json displays in table

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import React from 'react';
-import Employees from "./components/Employees"
+import Title from "./components/Title"
+import Employee from "./components/Employees";
 import './App.css';
+import employees from './employees.json';
 
 function App() {
   return (
-    <Employees />
-    // <div className="App">
-    //   <header className="App-header">
-    //     <p>
-    //       Employee Directory
-    //     </p>
-    //         </header>
-    // </div>
-    
+    <div>
+      <Title>Employees</Title>
+      <div className="container">
+        <Employee 
+        name={employees[0].name}
+        department={employees[0].department}
+        phone={employees[0].phone}
+        email={employees[0].email}/>
+      </div>
+    </div>
   );
 }
 
--- src/components/Employees/index.js
+++ src/components/Employees/index.js
@@ -4,28 +4,28 @@ import "./style.css";
 function Employee(props) {
   return (
 
-   
+
     <div>
-       <h1>Test</h1>
-    <table class="table">
-    <thead>
-      <tr>
-        <th scope="col">Name</th>
-        <th scope="col">Department</th>
-        <th scope="col">Phone Number</th>
-        <th scope="col">Email</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <td>test</td>
-        <td>test department</td>
-        <td>1234543</td>
-        <td>abcd@email.com</td>
-      </tr>
-    </tbody>
-  </table>
-  </div>
+      <h1>Test</h1>
+      <table class="table">
+        <thead>
+          <tr>
+            <th scope="col">Name</th>
+            <th scope="col">Department</th>
+            <th scope="col">Phone Number</th>
+            <th scope="col">Email</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>{props.name}</td>
+            <td>{props.department}</td>
+            <td>{props.phone}</td>
+            <td>{props.email}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   );
 }
 
