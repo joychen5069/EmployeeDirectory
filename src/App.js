9e889bca6961c7f6518d0beabb0b8f6c181b09af
Joy Chen <joychen5069@gmail.com>
2020-07-04 13:52:01 -0400

    removed broken sort function

--- src/App.js
+++ src/App.js
@@ -14,100 +14,50 @@ class App extends Component {
   };
 
   sortName = () => {
-    
+
     const { filteredEmployees, ascending } = this.state
     console.log(filteredEmployees);
     console.log(ascending);
     const direction = ascending ? 'asc' : 'desc'
-   const sorted = orderBy(filteredEmployees,'name', direction);
-  console.log(sorted)
-  this.setState({filteredEmployees: sorted, ascending:!ascending})
+    const sorted = orderBy(filteredEmployees, 'name', direction);
+    console.log(sorted)
+    this.setState({ filteredEmployees: sorted, ascending: !ascending })
 
 
     console.log("clicked")
-  //   let table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
-  // table = document.getElementById("table");
-  // switching = true;
-  // //Set the sorting direction to ascending:
-  // dir = "asc"; 
-  // /*Make a loop that will continue until
-  // no switching has been done:*/
-  // while (switching) {
-  //   //start by saying: no switching is done:
-  //   switching = false;
-  //   rows = table.rows;
-  //   /*Loop through all table rows (except the
-  //   first, which contains table headers):*/
-  //   for (i = 1; i < (rows.length - 1); i++) {
-  //     //start by saying there should be no switching:
-  //     shouldSwitch = false;
-  //     /*Get the two elements you want to compare,
-  //     one from current row and one from the next:*/
-  //     x = rows[i].getElementsByTagName("td")[n];
-  //     y = rows[i + 1].getElementsByTagName("td")[n];
-  //     /*check if the two rows should switch place,
-  //     based on the direction, asc or desc:*/
-  //     if (dir === "asc") {
-  //       if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
-  //         //if so, mark as a switch and break the loop:
-  //         shouldSwitch= true;
-  //         break;
-  //       }
-  //     } else if (dir === "desc") {
-  //       if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
-  //         //if so, mark as a switch and break the loop:
-  //         shouldSwitch = true;
-  //         break;
-  //       }
-  //     }
-  //   }
-  //   if (shouldSwitch) {
-  //     /*If a switch has been marked, make the switch
-  //     and mark that a switch has been done:*/
-  //     rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
-  //     switching = true;
-  //     //Each time a switch is done, increase this count by 1:
-  //     switchcount ++;      
-  //   } else {
-  //     /*If no switching has been done AND the direction is "asc",
-  //     set the direction to "desc" and run the while loop again.*/
-  //     if (switchcount === 0 && dir === "asc") {
-  //       dir = "desc";
-  //       switching = true;
-  //     }
-  //   }
+
   }
-  
+
 
   render() {
     return (
       <>
         <Title>Employee Directory</Title>
-        
+
         <div className="container">
-        <Form />
-        <table className="table" id="table">
-        <thead>
-          <tr className="header">
-            <th scope="col" onClick={this.sortName}>Name</th>
-            <th scope="col">Department</th>
-            <th scope="col" >Phone Number</th>
-            <th scope="col">Email</th>
-          </tr>
-        </thead>
-          {this.state.filteredEmployees.map(employee => {
-            return (
-              <Employee
-                id={employee.id}
-                key={employee.id}
-                name={employee.name}
-                department={employee.department}
-                phone={employee.phone}
-                email={employee.email}
-              />
-            )
-          })}
-            </table>
+          <Form />
+          <table className="table" id="table">
+            <thead>
+              <tr className="header">
+                <th scope="col" onClick={this.sortName}>Name</th>
+                <th scope="col">Department</th>
+                <th scope="col" >Phone Number</th>
+                <th scope="col">Email</th>
+              </tr>
+            </thead>
+            {this.state.filteredEmployees.map(employee => {
+              return (
+                <Employee
+                  id={employee.id}
+                  key={employee.id}
+                  name={employee.name}
+                  department={employee.department}
+                  phone={employee.phone}
+                  email={employee.email}
+                />
+              )
+            })}
+          </table>
         </div>
       </>
     );
